b71cdd1873d6b6674f596ef52646048e7d9c055c
Daniel Sliwinski <daniel.sliwinski.00@gmail.com>
2023-05-02 15:28:44 +0100

    minor changes

--- screens/menu.js
+++ screens/menu.js
@@ -23,7 +23,7 @@ export default class Menu extends Component {
     }
 
     getMenu() {
-        return fetch("http://192.168.1.102:8080/api/menu.php",
+        return fetch("http://192.168.1.102:8080/final-year-project-php-api/menu.php",
             {
                 headers: { 'Content-Type': 'application/json' }
             })
